191ca7d678746a2382f30ecb0e93f5b039fb0de1
ThanabutJ <thanabut.j@gmail.com>
2021-07-25 18:24:57 +0700

    add basic template for index route

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 
+	r.Static("/assets", "./assets")
+
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Index",
+		})
+	})
+
+	r.Run()
 }
